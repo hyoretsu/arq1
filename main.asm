3f8c1620c35a2cd0f51b372dd768f9f566b91f25
Aran Leite <hyoretsu@gmail.com>
2023-05-19 18:04:36 -0300

    Projeto: tratamento de opção (inválida e dummy)

--- main.asm
+++ main.asm
@@ -2,11 +2,14 @@ extern printf
 extern scanf
 
 section .data
+    invalidOptionMessage db "Comando não reconhecido, tente novamente.", 0AH, 0H
     menu db "----- Bem vindo à Cifra de César! -----", 0AH, "1) Criptografar", 0AH, "2) Descriptografar", 0AH, "3) Criptoanálise", 0AH, "4) Sair", 0AH, "Sua escolha: ", 0H
     option dd 0
 
     intFormat db "%d", 0H
     strFormat db "%s", 0H
+
+    sys_exit dw 0
 
 section .text
     global main
@@ -15,11 +18,24 @@ section .text
         call showMenu
         call getOption
 
+        mov eax, [option]
+        cmp eax, 1
+            jl invalidOption
+            je exit
+        cmp eax, 2
+            je exit
+        cmp eax, 3
+            je exit
+        cmp eax, 4
+            je exit
+            jg invalidOption
+
         ret
 
     showMenu:
         mov rdi, strFormat
         mov rsi, menu
+        mov rax, 0
         call printf
 
         ret
@@ -28,5 +44,20 @@ section .text
         mov rdi, intFormat
         mov rsi, option
         call scanf
+
+        ret
+
+    invalidOption:
+        mov rdi, strFormat
+        mov rsi, invalidOptionMessage
+        mov rax, 0
+        call printf
+
+        ret
+
+    exit:
+        mov ebx, sys_exit
+        mov eax, 1
+        int 80h
 
         ret
